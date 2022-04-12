243f7c3bba358d66003ff64497faec96c312ed87
hamza_rebb <zazarebba975@gmail.com>
2022-04-12 11:52:58 +0200

    updating the script

--- gitautom.sh
+++ gitautom.sh
@@ -5,6 +5,16 @@ read commitMessage
 
 git message "$commitMessage"
 
+echo 'Do you want to create a branch (Y/N) ? '
+read branchMessage
+
+if [$branchMessage = "Y"]
+then
+    echo 'Enter the branch name: '
+    read branchName
+    git branch "$branchName"
+fi
+
 echo 'Enter the name of the branch: '
 read branch
 
