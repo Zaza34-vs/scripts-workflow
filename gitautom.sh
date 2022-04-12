8088c64adb92dc0bad01f4532cfff606b621363b
hamza_rebb <zazarebba975@gmail.com>
2022-04-12 12:12:22 +0200

    returning to a version who will be working

--- gitautom.sh
+++ gitautom.sh
@@ -5,20 +5,5 @@ read commitMessage
 
 git message "$commitMessage"
 
-echo 'Do you want to create a branch (Y/N) ? '
-read branchMessage
-
-if [$branchMessage == 'Y']
-then
-    echo 'Enter the branch name: '
-    read branchName
-    git br "$branchName"
-
-elif [$branchMessage == 'N']
-then
-    git psh 
-fi
-
-
-# 'Enter the branch name: '
-# git psh origin $branch
+'Enter the branch name: '
+git psh origin $branch
---
+++ ifstatement.sh
@@ -0,0 +1,13 @@
+# echo 'Do you want to create a branch (Y/N) ? '
+# read branchMessage
+
+# if [$branchMessage == 'Y']
+# then
+#     echo 'Enter the branch name: '
+#     read branchName
+#     git br "$branchName"
+
+# elif [$branchMessage == 'N']
+# then
+#     git psh 
+# fi
